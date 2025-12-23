7e1998d7670a9267aba8ace2e95ee6222677b512
Ramiro Urrutia <urrutiarami@gmail.com>
2025-12-23 18:40:50 -0300

    Refactor item result display and streamline scanning logic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,56 +1,52 @@
 "use client";
 import axios from "axios";
-import Html5QrcodePlugin from './components/qrScanner';
+import QrScanner from './components/qrScanner';
 import { useState } from "react";
-import Image from "next/image";
+
+export default function Home() {
 
 interface Item {
-  id: number;
-  code: string;
+  imageUrl: string;
   name: string;
+  code: string;
   category: string;
   status: string;
-  notes: string | null;
-  imageUrl: string | null;
+  notes: string;
 }
 
-function OnScan(params: string) {
-  const [item, setItem] = useState<Item | null>(null);
+const [scanResult, setScanResult] = useState<Item | null>(null);
 
-  return (<>
-    {item ? (
-      <div>
-        <h2>Item Details</h2>
-        <p><strong>Name:</strong> {item.name}</p>
-        <p><strong>Category:</strong> {item.category}</p>
-        <p><strong>Status:</strong> {item.status}</p>
-        <p><strong>Notes:</strong> {item.notes}</p>
-        {item.imageUrl && <Image src={item.imageUrl} alt={item.name} />}
-      </div>) : (<p>Loading item data...</p>
-    )}
-  </>);
-}
+const ItemResult = ({data} : {data: Item}) => (
+  <div>
+    <h2>Scan Result:</h2>
+    <p>Image URL: {data.imageUrl}</p>
+    <p>Name: {data.name}</p>
+    <p>Code: {data.code}</p>
+    <p>Category: {data.category}</p>
+    <p>Status: {data.status}</p>
+    <p>Notes: {data.notes}</p>
+  </div>
+);
 
 const onNewScanResult = async (decodedText: string) => {
   try {
     const res = await axios.post("http://localhost:4000/scan", {
       code: decodedText
     });
-    
+    setScanResult(res.data);
   } catch (err) {
     console.error("Error sending scan result:", err);
   } finally {
   }
 };
 
-
-export default function Home() {
   return (
     <main>
       <h1>Welcome to the Stock Sonido App</h1>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Scan Item</button>
-      <Html5QrcodePlugin
+      <QrScanner
         qrCodeSuccessCallback={onNewScanResult} />
+      {scanResult ? <ItemResult data={scanResult} /> : <p>No scan result yet.</p>}
     </main>
   );
 }
